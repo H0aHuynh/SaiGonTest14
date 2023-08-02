52c54e8c0d262f754da462355a228215ae9a0b9f
Hòa Huỳnh <45682586+H0aHuynh@users.noreply.github.com>
2023-08-02 21:35:08 +0700

    up

--- SaiGonTest/ViewController.m
+++ SaiGonTest/ViewController.m
@@ -7,6 +7,58 @@
 
 #import "ViewController.h"
 #include "mycommon.h"
+#include "../utils/patchfinder64.h"
+#include "../utils/kerneldec.h"
+#include "../utils/utils.h"
+uint64_t allprocfind;
+int kernel(void){
+    int rv;
+    uint64_t base = 0;
+    NSError* error = nil;
+    
+    NSString* prebootPath = @"/private/preboot/active";
+    
+    NSString* activeFolderName = [NSString stringWithContentsOfFile:prebootPath];
+    
+    NSString* kernelPath = [NSString stringWithFormat:@"/private/preboot/%@/System/Library/Caches/com.apple.kernelcaches/kernelcache", activeFolderName];
+    
+    NSString* workspaceKernelPath = @"/tmp/kernel.tmp";
+    
+    [[NSFileManager defaultManager] copyItemAtPath:kernelPath toPath:workspaceKernelPath error:&error];
+    
+    if(error){
+        util_error("Failed copy");
+        exit(1);
+    }
+    
+    FILE* file_input = fopen([workspaceKernelPath UTF8String], "rb");
+    FILE* file_output = fopen("/tmp/kernel.dec.tmp", "wb");
+    
+    if(file_output == NULL || file_input == NULL){
+        util_error("Failed open /tmp/kernel.tmp");
+        exit(1);
+    }
+    
+    decompress_kernel(file_input, file_output, NULL, true);
+    
+    fclose(file_input);
+    fclose(file_output);
+    
+    
+    
+    init_kernel(0, "/tmp/kernel.dec.tmp");
+ //   assert(rv == 0);
+    allprocfind = find_cs_blob_generation_count() + g_exp.kernel_slide;
+    term_kernel();
+    if (allprocfind != g_exp.kernel_slide){
+        util_error("Error: patchfinder failed\n");
+    }
+    util_info("find_cs_blob_generation_count: 0x%llx\n", allprocfind);
+    unlink("/tmp/kernel.tmp");
+    unlink("/tmp/kernel.dec.tmp");
+    return 0;
+}
+
 
 extern void (*log_UI)(const char *text);
 void log_toView(const char *text);
@@ -66,6 +118,7 @@ char *Build_resource_path(char *filename)
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         void exploit_main(void);
         exploit_main();
+        kernel();
         dispatch_sync( dispatch_get_main_queue(), ^{
             [[sharedController goButton] setTitle:enjoyStr forState:UIControlStateNormal];
             [[sharedController goButton] setEnabled:TRUE];
